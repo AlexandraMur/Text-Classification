d720dd46c0ad25dc00af14834b22a047adfdc531
Sasha <murzina93@gmail.com>
2016-05-22 11:24:01 +0600

    add new functionality

--- training.py
+++ training.py
@@ -14,19 +14,37 @@ def get_weights(common_words, category):
         term_frequency = value / common_weight
 
         number_documents_in_category, Number_documents = normalization.get_frequency_of_word_among_all(key, category)
-        inverse_document_frequency = math.log2(Number_documents / number_documents_in_category)
+        inverse_document_frequency = math.log2((Number_documents / number_documents_in_category if number_documents_in_category != 0 else 1) if Number_documents/number_documents_in_category != 0 else 1)
 
         weights.append(('"'+key+'"', term_frequency * (1 if inverse_document_frequency == 0 else inverse_document_frequency)))
 
     print(weights)
     return weights
 
-def compare_weight(weights, weights_from_db):
-    return
+def compare_weight(weights, category):
+    state, weights_from_db = db.get_category_data(category)
+
+    if state is False:
+        return
+
+    weights_from_db = dict(weights_from_db)
+    weights_ = dict(weights.copy())
+
+    for key, value in weights:
+        key_ = key.split('"')[1]
+        try:
+            value_from_db = weights_from_db[key_]
+            if value_from_db != value:
+                weights_[key] = (value + value_from_db) / 2
+        except Exception:
+            continue
+
+    return list(weights_.items())
 
 def train(common_words, category, is_new=False):
     if (is_new):
         db.create_new_category(category)
 
     words_and_weights = get_weights(common_words, category)
+    words_and_weights = compare_weight(words_and_weights, category)
     db.set_category_data(category, words_and_weights)
